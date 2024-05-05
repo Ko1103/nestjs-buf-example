287c4bc96b6a89d76288ab0d28fd2c48ae91bebc
山浦功 <ko.foot1103@gmail.com>
2024-05-05 17:02:57 +0900

    fix: type error

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,19 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { DateTime } from 'src/proto/google/type/datetime_pb';
+import { GetUserResponse } from 'src/proto/user/v1/user_pb';
 import { UserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UsersController {
   @GrpcMethod('UserService', 'GetUser')
-  getUser(): UserDto[] {
-    return [
-      new UserDto({ userId: '1', name: 'John', createdAt: new DateTime() }),
-    ];
+  getUser(): GetUserResponse {
+    return new GetUserResponse({
+      user: new UserDto({
+        userId: '1',
+        name: 'John',
+        createdAt: new DateTime(),
+      }),
+    });
   }
 }
